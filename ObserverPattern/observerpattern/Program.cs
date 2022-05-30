a11b830e51cc310697a8bd195606bb1ad95628ee
isyankarcoder <gencvolkan@gmail.com>
2022-05-29 20:44:09 -0700

    observer

---
+++ ObserverPattern/observerpattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace observerpattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
